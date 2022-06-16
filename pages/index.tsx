c76ad8f324fbba075a6e1e555cf9597b11f44ba8
Antonio Herrera <antonio@bioscout.com.au>
2022-06-16 20:31:35 +1000

    Finish basic implementation. Add mq3 signature <3

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,112 +2,165 @@ export default function Home() {
   return (
     <>
       <main>
-        <div className="w-full h-screen flex justify-center items-center bg-black">
+        <div className="w-full py-16 flex justify-center items-center bg-primary">
           <div className="text-center mt-8">
-            <h1 className="text-white">
-              NUEVA LEY DE DEPORTE ADAPTADO INCLUSIVO Y PARALÍMPICO.
+            <img
+              className="w-10/12 md:w-4/12 mx-auto"
+              src="/logo.png"
+              alt="Logo"
+            />
+            <h1 className="text-white text-2xl md:text-3xl pt-8 ">
+              Una nueva ley para que el <br />
+              <b>deporte sea un derecho</b> <br />
+              que nos incluya a todos.
             </h1>
-            <h2 className="text-white">
-              Mientras tanto, enterate de todo en nuestras redes sociales
-            </h2>
-
-            <div className="w-full text-center mt-8">
-              <a href="https://instagram.com/lamayorminoria">
-                <img
-                  className="inline-block w-12"
-                  src="/instagram.svg"
-                  alt=""
-                />
-              </a>
-            </div>
           </div>
         </div>
-        <div className="w-10/12 md:w-8/12 lg:w-6/12 mx-auto py-12">
-          <h1 className="text-primary text-3xl">PROPUESTA</h1>
-          <p className="mt-6">
-            <b>1. ENTIDADES OFICIALES RECONOCIDAS:</b> Que se reconozcan a las
-            entidades oficiales que trabajan en el deporte adaptado para que
-            puedan aplicar los programas nacionales y provinciales como también
-            poder ser ejecutores directos, teniendo en cuenta que las 24
-            provincias de Argentina tienen un área de deporte adaptado dentro de
-            cada órgano de Gobierno que rige el deporte.
-          </p>
-          <p className="mt-6">
-            <b>2. PRESTACIONES DEPORTIVAS:</b> Se propone agregar el item:
-            “Prestaciones Deportivas el artículo 17 bis a la ley 24.901 que
-            regula las “Prestaciones Básicas” a través Nomenclador Aranceles
-            Sistema Único. Reconociendo a la actividad física y el deporte como
-            una prestación significativa en el proceso de rehabilitación e
-            integración e inclusión en la sociedad. Los programas deportivos que
-            se desarrollen deberán estar inscriptos y supervisados por el
-            organismo oficial competente que correspondiere y los profesionales
-            deben estar formados certificados como Educadores Deportivos bajo el
-            contralor de los entes implicados del sistema de salud y el sistema
-            deportivo y dentro de un plan de capacitación continua.
-          </p>
-          <p className="mt-6">
-            <b>3. CURRICULA EDUCATIVA:</b> Que se agregue en la curricula
-            educativa desde nivel inicial hasta el nivel terciario/
-            universitario el abordaje del deporte adaptado, paralímpico e
-            inclusivo en todas sus dimensiones teóricas y prácticas garantizando
-            la práctica en el campo con al menos el 60% de la carga horaria
-            total.
-            <br />
-          </p>
-          <p className="mt-6">
-            <b>4. FORMACION Y CAPACITACIÒN PROFESIONAL:</b> Crear programas
-            vinculados a la formación y capacitación profesional articulando el
-            sistema de salud, deportivo, educativo y académico, promoviendo la
-            figura del EDUCADOR DEPORTIVO, de esta manera no perderemos el Saber
-            Práctico de todos los expertos, profesionales e idóneos que trabajan
-            en cada ámbito.
-          </p>
-          <p className="mt-6">
-            <b>5. EQUIPAMIENTO DEPORTIVO Y ELEMENTOS DEPORTIVOS:</b> Proponemos
-            crear un programa de asistencia técnica específica en el deporte
-            para la adquisición de equipamiento deportivo y elementos deportivos
-            para asistir desde las etapas de iniciación, desarrollo y alto
-            rendimiento en el deporte, ya que de esta manera habría más
-            posibilidades de igualdad en el campo, no podemos llegar en etapas
-            juveniles o mayores para que obtengan su equipamiento ya que estamos
-            llegando tarde social y deportivamente.
-            <br />
-          </p>
-          <p className="mt-6">
-            <b>6. INFRAESTRUCTURA DEPORTIVA y DISEÑO UNVERSAL:</b> Proponemos
-            crear un programa específicamente a infraestructura deportiva y que
-            se asigne a nivel presupuestario en la misma proporción de la
-            población con discapacidad según datos del INDEC, facilitando la
-            transformación de nuestro contexto hacia el Diseño Universal.
-            <br />
-          </p>
-          <p className="mt-6">
-            <b>7. TRANSPORTACION TERRESTRE, AEREA y OTRO:</b> PQue se aumente el
-            porcentaje de transportación de deportistas en los medios
-            interurbanos y de larga distancia según la capacidad de la unidad en
-            una proporción equivalente a los datos del INDEC, También proponemos
-            una bonificación en el costo total para posibilitar igualar
-            oportunidades de uso en la transportación aérea, coches cama a nivel
-            terrestre, marítima, y otros.
-          </p>
-          <p className="mt-6">
-            <b>8. FORMACION UNIVERSITARIA:</b> Generar un programa de beneficios
-            institucionales para las casas de formación otorgando a las personas
-            con discapacidad una bonificación en los aranceles de cada carrera.
-          </p>
-          <p className="mt-6">
-            <b>9. FAMILIA, DEPORTE Y VIDA SANA:</b> Proponemos crear una
-            programa en red a través de los centros de rehabilitación y los
-            entes deportivos oficiales donde se atienda la problemática del
-            sedentarismo, la obesidad para su abordaje desde la niñez hasta los
-            adultos mayores.
-          </p>
-          <p className="mt-6">
-            <b> 10. DEPORTE Y TECNOLOGIA:</b> Crear programas que estimulen al
-            sector del desarrollo tecnológico de todos niveles a los fines de
-            crear dispositivos que faciliten la igualdad de oportunidades
-            facilitando al mismo tiempo un contexto más accesible para todos.
-          </p>
+        <div className="w-full py-16 flex justify-center items-center bg-white">
+          <div className="text-center mt-8">
+            <h1 className="text-primary text-2xl md:text-3xl lg:text-5xl pt-8 font-extrabold">
+              NUEVA LEY DE DEPORTE ADAPTADO <br />
+              INCLUSIVO Y PARALÍMPICO.
+            </h1>
+            <p className="py-12 text-2xl text-center w-10/12 md:w-6/12 lg:w-4/12 mx-auto">
+              “El deporte como medio para la mejora física, cognitiva y de
+              integración social, es un factor clave para nuestros hijos. Sin
+              una ley que los proteja, lamentablemente sus posibilidades reales
+              de evolución se ven fuertemente reducidas”
+            </p>
+
+            <h5 className="pb-8 text-center text-2xl font-bold">
+              Daniela, mamá de Fede
+            </h5>
+          </div>
+        </div>
+        <div className="bg-secondary py-20">
+          <h4 className="text-white text-2xl md:text-4xl text-center w-10/12 md:w-8/12 mx-auto">
+            El deporte es una herramienta <b>fundamental</b> en el proceso de
+            rehabilitación, integración e inclusión en la sociedad.
+          </h4>
+        </div>
+        <div className="w-full pt-6 pb-4 flex justify-center items-center bg-gray-50">
+          <div className="w-10/12 md:w-8/12 lg:w-6/12 mx-auto py-12">
+            <h1 className="text-primary text-5xl font-bold text-center pb-6">
+              PROPUESTA
+            </h1>
+            <p className="mt-6">
+              <b>1. ENTIDADES OFICIALES RECONOCIDAS:</b> Que se reconozcan a las
+              entidades oficiales que trabajan en el deporte adaptado para que
+              puedan aplicar los programas nacionales y provinciales como
+              también poder ser ejecutores directos, teniendo en cuenta que las
+              24 provincias de Argentina tienen un área de deporte adaptado
+              dentro de cada órgano de Gobierno que rige el deporte.
+            </p>
+            <p className="mt-6">
+              <b>2. PRESTACIONES DEPORTIVAS:</b> Se propone agregar el item:
+              “Prestaciones Deportivas el artículo 17 bis a la ley 24.901 que
+              regula las “Prestaciones Básicas” a través Nomenclador Aranceles
+              Sistema Único. Reconociendo a la actividad física y el deporte
+              como una prestación significativa en el proceso de rehabilitación
+              e integración e inclusión en la sociedad. Los programas deportivos
+              que se desarrollen deberán estar inscriptos y supervisados por el
+              organismo oficial competente que correspondiere y los
+              profesionales deben estar formados certificados como Educadores
+              Deportivos bajo el contralor de los entes implicados del sistema
+              de salud y el sistema deportivo y dentro de un plan de
+              capacitación continua.
+            </p>
+            <p className="mt-6">
+              <b>3. CURRICULA EDUCATIVA:</b> Que se agregue en la curricula
+              educativa desde nivel inicial hasta el nivel terciario/
+              universitario el abordaje del deporte adaptado, paralímpico e
+              inclusivo en todas sus dimensiones teóricas y prácticas
+              garantizando la práctica en el campo con al menos el 60% de la
+              carga horaria total.
+              <br />
+            </p>
+            <p className="mt-6">
+              <b>4. FORMACION Y CAPACITACIÒN PROFESIONAL:</b> Crear programas
+              vinculados a la formación y capacitación profesional articulando
+              el sistema de salud, deportivo, educativo y académico, promoviendo
+              la figura del EDUCADOR DEPORTIVO, de esta manera no perderemos el
+              Saber Práctico de todos los expertos, profesionales e idóneos que
+              trabajan en cada ámbito.
+            </p>
+            <p className="mt-6">
+              <b>5. EQUIPAMIENTO DEPORTIVO Y ELEMENTOS DEPORTIVOS:</b>{' '}
+              Proponemos crear un programa de asistencia técnica específica en
+              el deporte para la adquisición de equipamiento deportivo y
+              elementos deportivos para asistir desde las etapas de iniciación,
+              desarrollo y alto rendimiento en el deporte, ya que de esta manera
+              habría más posibilidades de igualdad en el campo, no podemos
+              llegar en etapas juveniles o mayores para que obtengan su
+              equipamiento ya que estamos llegando tarde social y
+              deportivamente.
+              <br />
+            </p>
+            <p className="mt-6">
+              <b>6. INFRAESTRUCTURA DEPORTIVA y DISEÑO UNVERSAL:</b> Proponemos
+              crear un programa específicamente a infraestructura deportiva y
+              que se asigne a nivel presupuestario en la misma proporción de la
+              población con discapacidad según datos del INDEC, facilitando la
+              transformación de nuestro contexto hacia el Diseño Universal.
+              <br />
+            </p>
+            <p className="mt-6">
+              <b>7. TRANSPORTACION TERRESTRE, AEREA y OTRO:</b> PQue se aumente
+              el porcentaje de transportación de deportistas en los medios
+              interurbanos y de larga distancia según la capacidad de la unidad
+              en una proporción equivalente a los datos del INDEC, También
+              proponemos una bonificación en el costo total para posibilitar
+              igualar oportunidades de uso en la transportación aérea, coches
+              cama a nivel terrestre, marítima, y otros.
+            </p>
+            <p className="mt-6">
+              <b>8. FORMACION UNIVERSITARIA:</b> Generar un programa de
+              beneficios institucionales para las casas de formación otorgando a
+              las personas con discapacidad una bonificación en los aranceles de
+              cada carrera.
+            </p>
+            <p className="mt-6">
+              <b>9. FAMILIA, DEPORTE Y VIDA SANA:</b> Proponemos crear una
+              programa en red a través de los centros de rehabilitación y los
+              entes deportivos oficiales donde se atienda la problemática del
+              sedentarismo, la obesidad para su abordaje desde la niñez hasta
+              los adultos mayores.
+            </p>
+            <p className="mt-6">
+              <b> 10. DEPORTE Y TECNOLOGIA:</b> Crear programas que estimulen al
+              sector del desarrollo tecnológico de todos niveles a los fines de
+              crear dispositivos que faciliten la igualdad de oportunidades
+              facilitando al mismo tiempo un contexto más accesible para todos.
+            </p>
+
+            <h2 className="text-center text-3xl text-primary pt-16">
+              ¡Gracias por haber llegado hasta acá!
+            </h2>
+          </div>
+        </div>
+        <div className="bg-primary py-8">
+          <h4 className="text-white text-2xl text-center w-10/12 md:w-6/12 lg:w-4/12 mx-auto">
+            Seguinos y ayudanos con tu difusión a hacer realidad esta ley
+          </h4>
+          <div className="w-full text-center mt-8">
+            <a href="https://instagram.com/lamayorminoria">
+              <img className="inline-block w-12" src="/instagram.svg" alt="" />
+            </a>
+          </div>
+        </div>
+        <div className="w-full flex justify-center text-xs bg-gray-200 text-black">
+          <a
+            href="https://www.menorque3.com.ar/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Con amor de{' '}
+            <img
+              src="/mq3.png"
+              className="w-24 inline-block"
+              alt="menor que tres logo"
+            />
+          </a>
         </div>
       </main>
     </>
